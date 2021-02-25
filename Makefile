ed21c0ec15b44166f8085183c47ee75230d274f8
Galen Guyer <galen@galenguyer.com>
2021-02-24 19:05:53 -0500

    Add REPL loop

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS=-std=c99 -fsanitize=address -fno-omit-frame-pointer -Wall -Wextra -ggdb -O2
+CFLAGS=-std=c99 -fsanitize=address -fno-omit-frame-pointer -Wall -Wextra -ggdb -O2 -ledit
 
 dice: dice.c
 	gcc $(CFLAGS) -o dice dice.c
--- dice.c
+++ dice.c
@@ -1,5 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
-    puts("Hello, World!");
+    while(1) {
+        char* input = readline("dice> ");
+        add_history(input);
+        printf("%s\n", input);
+        free(input);
+    }
 }
