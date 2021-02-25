ece82051d580bf9f8b183f104d1da704476b7216
Galen Guyer <galen@galenguyer.com>
2021-02-24 19:28:49 -0500

    print very simple AST

--- dice.c
+++ dice.c
@@ -2,11 +2,32 @@
 #include <stdio.h>
 #include <editline/readline.h>
 
-int main(int argc, char** argv) {
+#include "mpc.h"
+
+int main() {
+    mpc_parser_t* Die = mpc_new("die");
+    mpc_parser_t* Dice = mpc_new("dice");
+
+    mpca_lang(MPCA_LANG_DEFAULT, 
+        "                           \
+            die  : /d[0-9][0-9]*/;  \
+            dice : /^/ <die> /$/;   \
+        ", Die, Dice);
+
     while(1) {
         char* input = readline("dice> ");
         add_history(input);
-        printf("%s\n", input);
+
+        mpc_result_t result;
+        if (mpc_parse("<stdin>", input, Dice, &result)) {
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        }
+        else {
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
+
         free(input);
     }
 }
