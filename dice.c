b8d4ed07604b7bafe9a2d19c3f209d7edccf39d3
Galen Guyer <galen@galenguyer.com>
2021-02-24 20:27:59 -0500

    Get rid of REPL loop for now

--- dice.c
+++ dice.c
@@ -21,43 +21,23 @@ int main(int argc, char* argv[]) {
         mpc_err_print(err);
     }
 
-    if (argc == 1) {
-        while(1) {
-            char* input = readline("dice> ");
-            add_history(input);
-
-            mpc_result_t result;
-            if (mpc_parse("<stdin>", input, Dice, &result)) {
-                mpc_ast_print(result.output);
-                mpc_ast_delete(result.output);
-            }
-            else {
-                mpc_err_print(result.error);
-                mpc_err_delete(result.error);
-            }
-
-            free(input);
+    char input[1024];
+    int i = 0;
+    for (int arg = 1; arg < argc; arg++) {
+        for (int pos = 0; argv[arg][pos] != '\0'; pos++) {
+            input[i++] = argv[arg][pos];
         }
     }
+    input[i] = '\0';
+
+    mpc_result_t result;
+    if (mpc_parse("<stdin>", input, Dice, &result)) {
+        mpc_ast_print(result.output);
+        mpc_ast_delete(result.output);
+    }
     else {
-        char input[1024];
-        int i = 0;
-        for (int arg = 1; arg < argc; arg++) {
-            for (int pos = 0; argv[arg][pos] != '\0'; pos++) {
-                input[i++] = argv[arg][pos];
-            }
-        }
-        input[i] = '\0';
-        
-        mpc_result_t result;
-        if (mpc_parse("<stdin>", input, Dice, &result)) {
-            mpc_ast_print(result.output);
-            mpc_ast_delete(result.output);
-        }
-        else {
-            mpc_err_print(result.error);
-            mpc_err_delete(result.error);
-        }
+        mpc_err_print(result.error);
+        mpc_err_delete(result.error);
     }
 
     free(Die);
